8c15aff90894f4093d291e71cc36b5ab0b1516d4
Preethipachiyappan <preethipachiyappan72@gmail.com>
2025-08-11 14:14:07 +0530

    first commit

---
+++ AI.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, send_file
+import cv2
+import numpy as np
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+PROCESSED_FOLDER = 'static/processed/'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(PROCESSED_FOLDER, exist_ok=True)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
+
+@app.route('/')
+def home():
+    return '''
+    <!doctype html>
+    <html>
+    <head>
+        <title>Image to Pencil Sketch</title>
+        <style>
+            body {
+                background-color: rgb(255,192,203);
+                font-family: Arial, sans-serif;
+            }
+            h1 {
+                color: #333;
+            }
+            input[type="file"], input[type="submit"] {
+                margin: 10px;
+            }
+        </style>
+    </head>
+    <body>
+    <center>
+    <h1>Image To Pencil Sketch Project</h1>
+    <form method="post" action="/upload" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Upload">
+    </form>
+    </center>
+    </body>
+    </html>
+    '''
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        # Convert the image to a pencil sketch
+        sketch_path = create_pencil_sketch(filepath, filename)
+
+        return f'''
+        <center>
+        <h1>Original Image</h1>
+        <img src="/{filepath}" width="300">
+        <h1>Pencil Sketch</h1>
+        <img src="/{sketch_path}" width="300">
+        <br>
+        <a href="/{sketch_path}" download>Download Pencil Sketch</a>
+        </center>
+        '''
+
+def create_pencil_sketch(input_path, filename):
+    image = cv2.imread(input_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    inverted_image = cv2.bitwise_not(gray_image)
+    blurred = cv2.GaussianBlur(inverted_image, (21, 21), sigmaX=0, sigmaY=0)
+    inverted_blur = cv2.bitwise_not(blurred)
+    sketch = cv2.divide(gray_image, inverted_blur, scale=256.0)
+
+    output_path = os.path.join(app.config['PROCESSED_FOLDER'], f'sketch_{filename}')
+    cv2.imwrite(output_path, sketch)
+    return output_path
+
+if __name__ == '__main__':
+    app.run(debug=True)
